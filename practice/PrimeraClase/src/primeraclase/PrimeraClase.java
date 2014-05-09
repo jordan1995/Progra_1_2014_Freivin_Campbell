4de4ed6f473687ba25b2c47827404b09c5c24cff
Freivincampbell <freivin12@gmail.com>
2014-05-09 14:58:54 -0600

    class 1
    this is the frist class

---
+++ practice/PrimeraClase/src/primeraclase/PrimeraClase.java
@@ -0,0 +1,50 @@
+/*
+ * Name: PrimeraClase
+ 
+ * Information:This is the frist class.
+  
+ * Date: 7/05/2014.
+  
+ * Copyrigh.
+ 
+ */
+package PrimeraClase;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Campbell
+ */
+public class PrimeraClase {
+
+    /**
+     * @param args the command line arguments thus is the main method
+     */
+    public static void main(String[] args) {
+        double horasLaboradas = 0;
+        double costoHora = 0;
+        double salario = 0;
+
+        Scanner teclado = new Scanner(System.in);
+
+        //JOptionPane.showMessageDialog(null, "Hello World");
+        System.out.print("Hello world" + "\n");
+
+        System.out.print("Digite las horas laboradas por el trabajador:");
+
+        horasLaboradas = Double.parseDouble(teclado.nextLine());
+
+        System.out.print("Digite el costo de las horas:");
+
+        costoHora = Double.parseDouble(teclado.nextLine());
+
+        salario = horasLaboradas * costoHora;
+
+        System.out.println("El salario final es de:" + " " + salario);
+
+        System.out.println("Java epic win" + "\n");
+
+    }
+}
